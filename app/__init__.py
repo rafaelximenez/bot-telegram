f8137efcb583478d09b01e328777a25414c98129
Rafael Ximenez <rximenez97@gmail.com>
2021-03-27 14:01:38 -0300

    adding conversation

--- app/__init__.py
+++ app/__init__.py
@@ -5,15 +5,17 @@ import qrcode
 
 from .helpers import ocr
 from .helpers import sentiment_analysis
-from telegram import Update
+from telegram import Update, ReplyKeyboardMarkup
 from telegram.bot import Bot
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import Updater, CommandHandler, MessageHandler, ConversationHandler, Filters, CallbackContext
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
 
 logger = logging.getLogger(__name__)
+
+SENTIMENT, QR = range(2)
 
 def send_photo(update: Update, _: CallbackContext):
     bot = Bot(token=os.environ['TELEGRAM_TOKEN'])
@@ -27,15 +29,29 @@ def send_photo(update: Update, _: CallbackContext):
         ocr_text, ocr_list = ocr.detect_text(filename)    
         update.message.reply_text(str(ocr_text))
     except:
-        update.message.reply_text('Desculpe! Eu não consigo converter essa imagem em texto')
+        update.message.reply_text('Oooops! Eu não consegui converter essa imagem em texto')
 
-def generate_qr(update: Update, _: CallbackContext):
+def listen_qr(update: Update, _: CallbackContext):
+    update.message.reply_text(
+        "Me manda o texto para que eu gere seu QR."
+    )
+
+    return QR
+
+def listen_sentiment(update: Update, _: CallbackContext):
+    update.message.reply_text(
+        "Diga algo..."
+    )
+
+    return SENTIMENT
+
+def generate_qr(update: Update, context: CallbackContext):
     if re.search('http', update.message.text):
         img = qrcode.make(update.message.text)
         filename = 'tmp/qr/image.jpg'
         img.save(filename)
         update.message.reply_photo(photo=open(filename, 'rb'))
-    pass
+    return ConversationHandler.END
 
 def analysis_sentiment(update: Update, _: CallbackContext):
     score, magnitude = sentiment_analysis.analyze_feeling(update.message.text)
@@ -52,14 +68,35 @@ def analysis_sentiment(update: Update, _: CallbackContext):
         update.message.reply_text('Sentimento neutro!')
     elif score >= -1 and score <= -0.4:
         update.message.reply_text('Sentimento negativo!')
+    else:
+        update.message.reply_text(f'Score: {round(score, 2)} - Magnitude: {round(magnitude, 2)}')
+    return ConversationHandler.END
 
 def main() -> None:
     updater = Updater(os.environ['TELEGRAM_TOKEN'])
     
     dispatcher = updater.dispatcher
+    
+    conv_qr = ConversationHandler(
+        entry_points=[CommandHandler("qr", listen_qr)],
+        states={
+            QR: [MessageHandler(Filters.text, generate_qr)],
+        },
+        fallbacks=[],   
+    )
+
+    conv_sentiment = ConversationHandler(
+        entry_points=[CommandHandler("sentimento", listen_sentiment)],
+        states={
+            SENTIMENT: [MessageHandler(Filters.text, analysis_sentiment)],
+        },
+        fallbacks=[],   
+    )
+
+    dispatcher.add_handler(conv_qr)
+    dispatcher.add_handler(conv_sentiment)
+
     dispatcher.add_handler(MessageHandler(Filters.photo, send_photo))
-    #dispatcher.add_handler(MessageHandler(Filters.text, generate_qr))
-    dispatcher.add_handler(MessageHandler(Filters.text, analysis_sentiment))
 
     updater.start_polling()
 
